385b438743971987fb8c75edcfe1df8db1df4944
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-05-15 08:41:16 +0900

    리팩토링

--- youtube/routes/users.js
+++ youtube/routes/users.js
@@ -4,63 +4,69 @@ router.use(express.json());
 
 const conn = require('../mariaDB'); // db connection 객체. conn은 connection을 줄인 것
 
-let db = new Map()
-
 // 로그인
 router.post('/login', (req, res) => {
-    const {email, password} = req.body;
-    
-    conn.query(`SELECT * FROM users WHERE email = ?`, email, (err, result) => {
+    const { email, password } = req.body
+
+    const sql = `SELECT * FROM users WHERE email = ?`
+    conn.query(sql, email, (err, result) => {
         if (err) throw err
-        console.log(result)
         if (result.length && result[0].password === password) {
-            res.json({message: 'Login success'});
+            res.json({ message: 'Login success' });
         } else {
-            res.status(401).json({message: 'email or password is incorrect'});
+            res.status(401).json({ message: 'email or password is incorrect' });
         }
     });
 })
 
 // 회원 가입
 router.post('/register', (req, res) => {
-    const {email, name, password, contact} = req.body;
-    
-    // email, name, password 모두 있으면
+    const { email, name, password, contact } = req.body
+
     if (email && name && password) {
-        conn.query(`INSERT INTO users (email, name, password, contact) VALUES (?, ?, ?, ?)`, [email, name, password, contact], (err, result) => {
+        const sql = `INSERT INTO users (email, name, password, contact) VALUES (?, ?, ?, ?)`
+        const values = [email, name, password, contact]
+
+        conn.query(sql, values, (err, result) => {
             if (err) throw err
-            res.json({message: `Welcome ${name}!`});
+            if (result.affectedRows) {
+                res.json({ message: `Welcome ${name}!` });
+            } else {
+                res.status(400).json({ message: 'Register failed' });
+            }
         });
-    }else {
-        res.status(400).json({message: 'Register failed'});
+    } else {
+        res.status(400).json({ message: 'incorrect input' })
     }
 })
 
 // route 메소드 사용해서 중복되는 URL 합치기
 router.route('/users')
     .get((req, res) => {    // 회원 정보 조회
-        const {email} = req.body;
-        conn.query(`SELECT * FROM users WHERE email = ?`, email, (err, result) => {
+        const { email } = req.body
+
+        const sql = `SELECT * FROM users WHERE email = ?`
+        conn.query(sql, email, (err, result) => {
             if (err) throw err
-            if (result.length) { 
+            if (result.length) {
                 res.send(result)
             } else {
-                res.status(404).json({message: 'User not found'});
+                res.status(404).json({ message: 'User not found' })
             }
         });
     })
     .delete((req, res) => {    // 회원 탈퇴
-        const {email} = req.body;
-        console.log(email)
-        conn.query(`DELETE FROM users WHERE email = ?`, email, (err, result) => {
+        const { email } = req.body
+        
+        const sql = `DELETE FROM users WHERE email = ?`
+        conn.query(sql, email, (err, result) => {
             if (err) throw err
-            console.log(result)
             if (result.affectedRows) {
-                res.json({message: 'User deleted'});
+                res.json({ message: 'User deleted' });
             } else {
-                res.status(404).json({message: 'Delete failed'});
+                res.status(404).json({ message: 'Delete failed' });
             }
         });
     })
 
-module.exports = router;
+module.exports = router
