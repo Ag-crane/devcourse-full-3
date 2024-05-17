66afc19aa1f2229ae16463a9e5784874cfd1a193
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-05-17 09:30:54 +0900

    dovenv 학습

--- Node_Base/demo-api/exception-demo.js
+++ Node_Base/demo-api/exception-demo.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const dovenv = require('dotenv');
+dovenv.config({ path: '../.env' });
+const port = process.env.PORT
 
 const fruits = [
     {id: 1, name: 'banana', color: 'yellow'},
--- Node_Base/demo-npm/demo-jwt.js
+++ Node_Base/demo-npm/demo-jwt.js
@@ -1,12 +1,15 @@
 var jwt = require('jsonwebtoken');
-var privateKey = 'secret123';
+var dotenv = require('dotenv');
+dotenv.config({ path: '../.env'});
+
+console.log(process.env.PRIVATE_KEY);
 
 // 서명 (토큰 생성) : 페이로드 + 비밀키 + 알고리즘(defalt: SHA256)
-var token = jwt.sign({ id: 'user123'}, privateKey);
+var token = jwt.sign({ id: 'user123' }, process.env.PRIVATE_KEY);
 console.log(token);
 
 // 검증 (토큰 확인) : 토큰 + 비밀키
-var decoded = jwt.verify(token, privateKey);
+var decoded = jwt.verify(token, process.env.PRIVATE_KEY);
 console.log(decoded); // { id: 'user123', iat: 1715903533 }
 
 // iat (issued at) : 토큰 발급 시간. => 토큰은 발행할 때마다 달라진다.
