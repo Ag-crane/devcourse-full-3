8f2d104583eb03a8b4d419697797d0f7483f6a73
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-05-23 10:32:57 +0900

    chore(Book-Shop): 프로젝트 초기 설정

---
+++ Book_Shop/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+
+app.listen(process.env.PORT, () => {
+  console.log('Server is running on port 9999');
+});
+
