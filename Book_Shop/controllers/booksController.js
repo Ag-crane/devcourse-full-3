d71df3bd9b1d9110876780a2064bb17dca555a1a
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-05-29 09:22:50 +0900

    feat(api): 신간 도서 조회 API 추가, 리팩토링

--- Book_Shop/controllers/booksController.js
+++ Book_Shop/controllers/booksController.js
@@ -5,32 +5,33 @@ const crypto = require('crypto');
 require('dotenv').config();
 
 const allBooks = (req, res) => {
-    const { categoryId } = req.query;
-
-    if (categoryId) {
-        const sql = `SELECT * FROM books WHERE category_id = ?`;
-        conn.query(sql, categoryId, (err, result) => {
-            if (err) throw err;
-            if (result.length) {
-                res.status(StatusCodes.OK).json(result);
-            } else {
-                res.status(StatusCodes.NOT_FOUND).json({ message: 'Category not found' });
-            }
-        });
-    } else {
-            
-            const sql = `SELECT * FROM books`;
-            conn.query(sql, (err, result) => {
-                if (err) throw err;
-                res.status(StatusCodes.OK).json(result);
-            });
+    const { categoryId, isNew } = req.query;
+    let sql = `SELECT * FROM books `; // 전체 도서
+    let values = [];
+    if (categoryId && isNew) { // 카테고리별 신간
+        sql += `WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();`
+        values = [categoryId];
+    } else if (categoryId) { // 카테고리별 전체 도서
+        sql += `WHERE category_id = ?`;
+        values = [categoryId];
+    } else if (isNew) { // 전체 신간
+        sql += `WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();`
     }
+
+    conn.query(sql, values, (err, result) => {
+        if (err) throw err;
+        if (result.length) {
+            res.status(StatusCodes.OK).json(result);
+        } else {
+            res.status(StatusCodes.NOT_FOUND).json({ message: 'Not found' });
+        }
+    });
 }
 
 const bookDetail = (req, res) => {
     const { id } = req.params;
 
-    const sql = `SELECT * FROM books WHERE id = ?`;
+    const sql = `SELECT * FROM books b LEFT JOIN categories c ON b.category_id = c.id WHERE b.id = ?`;
     conn.query(sql, id, (err, result) => {
         if (err) throw err;
         if (result.length) {
