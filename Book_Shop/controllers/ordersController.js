99c2ce502ef50ebb504bec86e17126d093f74a7d
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-06-06 10:47:34 +0900

    feat(api): 주문 목록 조회, 주문 상세 조회 API 구현

--- Book_Shop/controllers/ordersController.js
+++ Book_Shop/controllers/ordersController.js
@@ -13,7 +13,7 @@ const createOrder = async (req, res) => {
     const orderValues = [firstBook, totalQuantity, totalPrice, userId, deliveryId];
     const [orderResults] = await conn.promise().execute(orderSql, orderValues);
     const orderId = orderResults.insertId;
-    
+
     const itemsSql = `select book_id, quantity from cartItems WHERE id IN (?)`;
     const [orderItems] = await conn.promise().query(itemsSql, [items]);
     console.log(orderItems)
@@ -28,12 +28,29 @@ const createOrder = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ message: '주문 성공' });
 }
 
-const getOrders = (req, res) => {
-    res.json('주문 목록 조회');
+const getOrders = async (req, res) => {
+    const { userId } = req.body;
+
+    const sql = `SELECT o.id, created_at, address, receiver, contact, first_book, total_quantity, total_price
+                FROM orders o
+                JOIN delivery d ON o.delivery_id = d.id
+                WHERE user_id = 1`
+    const [results] = await conn.promise().execute(sql, [userId])
+    
+    res.status(StatusCodes.OK).json(results);
 }
 
-const getOrderDetail = (req, res) => {
-    res.json('주문 상세 조회');
+const getOrderDetail = async (req, res) => {
+    const { orderId } = req.params;
+
+    const sql = `SELECT b.id, title, author, price, quantity, category_name
+                FROM orderedBook ob
+                JOIN books b ON ob.book_id = b.id
+                JOIN categories c ON b.category_id = c.id
+                WHERE order_id = ?`
+    const [results] = await conn.promise().execute(sql, [orderId])
+
+    res.status(StatusCodes.OK).json(results);
 }
 
 module.exports = { createOrder, getOrders, getOrderDetail };
