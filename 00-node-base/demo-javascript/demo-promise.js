666b53b1155cf0b905831476e9cfd2eabc4e5992
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-06-07 09:20:53 +0900

    폴더명 정리

---
+++ 00-node-base/demo-javascript/demo-promise.js
@@ -0,0 +1,9 @@
+// Promise : 약속을 지키는 객체
+
+// 매개변수로 콜백함수를 받는다.
+// executor : resolve, reject 두 개의 콜백함수를 매개변수로 가지는 콜백함수
+// 할 일을 성공적으로 마치면 resolve 호출
+// 실패하면 reject 호출
+let promise = new Promise((resolve, reject) => {
+    
+});
--- Node_Base/demo-javascript/demo-promise.js
+++
@@ -1,6 +0,0 @@
-
-async function f(){
-    return 1;
-}
-
-f().then(console.log); // 1
