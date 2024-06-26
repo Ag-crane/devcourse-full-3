b6df73998d8cfa5e53c713a3a41458ac3d9cd754
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-06-26 10:39:07 +0900

    Todo 데이터 타입 정의, 체크박스 기능 추가

--- 07-react-base/todolist/src/ToDoList.tsx
+++ 07-react-base/todolist/src/ToDoList.tsx
@@ -1,19 +1,43 @@
 import React, { useState } from 'react';
 
+
+type Todo = {
+    id: number;
+    text: string;
+    isChecked: boolean;
+};
+
 const ToDoList: React.FC = () => {
     const title: string = '오늘 할 일'
-    const [todos] = useState<string[]>(['운동하기', '아침먹기', '점심먹기', '저녁먹기'])
+    const [todos, setTodos] = useState<Todo[]>([
+        { id: 1, text: '아침 산책', isChecked: false },
+        { id: 2, text: '알고리즘 1문제 풀기', isChecked: false },
+        { id: 3, text: '운동', isChecked: false },
+    ])
+
+    const handleCheck = (id: number) => {
+        setTodos((prevTodos) =>
+            prevTodos.map((todo) => todo.id === id ? { ...todo, isChecked: !todo.isChecked } : todo))
+    }
     return (
         <div>
             <h1 className='title'>{title}</h1>
             <div className='board'>
                 <ul>
-                    {todos.map((todo, index) => (
-                        <li key={index}>{todo}</li>
+                    {todos.map((todo) => (
+                        <li key={todo.id}>
+                            <input type='checkbox' onChange={() => {
+                                handleCheck(todo.id);
+                            }} />
+                            <span>
+                                {
+                                    todo.isChecked ? <s>{todo.text}</s> : todo.text
+                                }
+                            </span>
+                        </li>
                     ))}
                 </ul>
             </div>
-
         </div>
     );
 };
