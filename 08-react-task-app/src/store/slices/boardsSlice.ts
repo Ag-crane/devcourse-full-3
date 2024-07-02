e0558442b469b630cea3327ee490b8da2f0d45bd
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-07-02 10:13:30 +0900

    Board 데이터 및 타입 생성

--- 08-react-task-app/src/store/slices/boardsSlice.ts
+++ 08-react-task-app/src/store/slices/boardsSlice.ts
@@ -1,8 +1,58 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { IBoard } from "../../types";
 
-const initialState = {
+type TBoardsState = {
+    modalActive: boolean;
+    boardArray: IBoard[];
+};
+
+const initialState: TBoardsState = {
     modalActive: false,
-    boatdArray: []
+    boardArray: [
+        {
+            boardId: "board-0",
+            boardName: "첫 번째 게시물",
+            lists: [
+                {
+                    listId: "list-0",
+                    listName: "List 1",
+                    tasks: [
+                        {
+                            taskId: "task-0",
+                            taskName: "Task 1",
+                            taskDescription: "Task 1 Description",
+                            taskOwner: "John"
+                        },
+                        {
+                            taskId: "task-1",
+                            taskName: "Task 2",
+                            taskDescription: "Task 2 Description",
+                            taskOwner: "John"
+                        }
+                    ]
+                },
+                {
+                    listId: "list-1",
+                    listName: "List 2",
+                    tasks: [
+                        {
+                            taskId: "task-2",
+                            taskName: "Task 3",
+                            taskDescription: "Task 3 Description",
+                            taskOwner: "John"
+                        },
+                        {
+                            taskId: "task-3",
+                            taskName: "Task 4",
+                            taskDescription: "Task 4 Description",
+                            taskOwner: "John"
+                        }
+                    ]
+                }
+                
+            ]
+        }
+    ]
 };
 
 const boardsSlice = createSlice({
--- 08-react-task-app/src/types/index.ts
+++ 08-react-task-app/src/types/index.ts
@@ -11,3 +11,15 @@ export interface IlogItem {
     logMessage: string;
     logTimestamp: string;
 }
+
+export interface IBoard {
+    boardId: string;
+    boardName: string;
+    lists: IList[];
+}
+
+export interface IList {
+    listId: string;
+    listName: string;
+    tasks: ITask[];
+}
