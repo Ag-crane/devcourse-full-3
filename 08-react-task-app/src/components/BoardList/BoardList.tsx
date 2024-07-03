7b2ef871c77430c4705a478a90086cbd13569d9a
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-07-03 09:20:59 +0900

    Board List 생성하기

--- 08-react-task-app/src/components/BoardList/BoardList.tsx
+++ 08-react-task-app/src/components/BoardList/BoardList.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { FC, useState } from "react";
+import { useTypedSelector } from "../../hooks/redux";
+import SideForm from "./SideForm/SideForm";
+import { FiPlusCircle } from "react-icons/fi";
 
-const BoardList = () => {
-  return (
-    <div>BoardList</div>
-  )
-}
+type TboardListProps = {
+    activeBoardId: string;
+    setActiveBoardId: React.Dispatch<React.SetStateAction<string>>;
+};
 
-export default BoardList
+const BoardList: FC<TboardListProps> = ({
+    activeBoardId,
+    setActiveBoardId,
+}) => {
+    const { boardArray } = useTypedSelector((state) => state.boards);
+    const [isFormOpen, setIsFormOpen] = useState(false);
+    return (
+        <div>
+            <div>게시판:</div>
+            {boardArray.map((board) => (
+                <div key={board.boardId}>
+                    <div>{board.boardName}</div>
+                </div>
+            ))}
+            <div>{isFormOpen ? <SideForm /> : <FiPlusCircle onClick={()=>setIsFormOpen(!isFormOpen)}/>}</div>
+        </div>
+    );
+};
+
+export default BoardList;
--- 08-react-task-app/src/store/slices/boardsSlice.ts
+++ 08-react-task-app/src/store/slices/boardsSlice.ts
@@ -51,6 +51,49 @@ const initialState: TBoardsState = {
                 }
                 
             ]
+        },
+        {
+            boardId: "board-1",
+            boardName: "두 번째 게시물",
+            lists: [
+                {
+                    listId: "list-0",
+                    listName: "List 1",
+                    tasks: [
+                        {
+                            taskId: "task-0",
+                            taskName: "Task 1",
+                            taskDescription: "Task 1 Description",
+                            taskOwner: "John"
+                        },
+                        {
+                            taskId: "task-1",
+                            taskName: "Task 2",
+                            taskDescription: "Task 2 Description",
+                            taskOwner: "John"
+                        }
+                    ]
+                },
+                {
+                    listId: "list-1",
+                    listName: "List 2",
+                    tasks: [
+                        {
+                            taskId: "task-2",
+                            taskName: "Task 3",
+                            taskDescription: "Task 3 Description",
+                            taskOwner: "John"
+                        },
+                        {
+                            taskId: "task-3",
+                            taskName: "Task 4",
+                            taskDescription: "Task 4 Description",
+                            taskOwner: "John"
+                        }
+                    ]
+                }
+                
+            ]
         }
     ]
 };
